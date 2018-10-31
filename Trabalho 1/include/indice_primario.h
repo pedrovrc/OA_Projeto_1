ecb8dfb9b17d22cb3b5ff7ce5189e87036320679
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2018-10-31 20:12:44 -0300

    Versão em desenvolvimento 31/10
    Índice primário funcional
    Índice secundário a terminar
    Structs base criadas

--- Trabalho 1/include/indice_primario.h
+++ Trabalho 1/include/indice_primario.h
@@ -1,4 +1,17 @@
 #include <stdio.h>
-#include <dirent.h>
 
-void gera_indice(FILE* referencia, char* nome_arquivo, char* nome_gerado);
+typedef struct Inicia_chave{
+	char chave[30];
+	struct Inicia_chave *prox;
+	int NRR;
+} inicia_chave;
+
+
+typedef struct Inicia_curso{
+	char curso[2];
+	struct Inicia_curso *prox;
+	inicia_chave *primeiro;
+} inicia_curso;
+
+
+int gera_indice_primario(FILE* arq_base, char* nome_gerado);
--- Trabalho 1/source/indice_primario.c
+++ Trabalho 1/source/indice_primario.c
@@ -4,33 +4,72 @@
 #include <string.h>
 #include <ctype.h>
 
-void gera_indice(FILE* referencia, char* nome_arquivo, char* nome_gerado) {
-    char linha_atual[70];
-    char string[30];
-    char caminho_arquivo[40] = "arquivos_principais/";
-    const int buffer = 70;
-    int tamanho;
-    int cont;
+#define TAM_REG 70
+#define TAM_CHAVE 30
+#define TAM_MATR_NOME 48
+#define TAM_DIR 80
+
+int gera_indice_primario(FILE* arq_base, char* nome_gerado) {
+    char linha_atual[TAM_REG];
+    char string[TAM_CHAVE];
+    char caminho_arquivo[TAM_DIR] = "arquivos_principais/";
+    int conta_char;
     int endereco_registro = 0;
+
+    if (arq_base == NULL) {
+        return 1;
+    }    
 
     strcat(caminho_arquivo, nome_gerado);
     FILE* indices = fopen(caminho_arquivo, "w");
-    
-    while (fgets(linha_atual, buffer, referencia) != NULL) {
-        for (int i = 0; i < 30; i++) {
+
+    if (indices == NULL) {
+        return 2;
+    }
+
+    while (fgets(linha_atual, TAM_REG, arq_base) != NULL) {
+        for (int i = 0; i < TAM_CHAVE; i++) {
             string[i] = ' ';
         }
-        endereco_registro++;        
-        cont = 0;
-        for (int i = 0; i < 48 && cont < 30; i++) {
+        conta_char = 0;
+        for (int i = 0; i < TAM_MATR_NOME && conta_char < TAM_CHAVE; i++) {
             if (isspace(linha_atual[i])) {
                 continue;
             }
-            string[cont] = linha_atual[i];
-            cont++;
+            string[conta_char] = linha_atual[i];
+            conta_char++;
         }
-        fprintf(indices, "%s %s", string, nome_arquivo);
+        fprintf(indices, "%s", string);
         fprintf(indices, " %d\n", endereco_registro);
+        endereco_registro++;
+    }
+
+    rewind(arq_base);
+    return 0;
+}
+
+
+int gera_indice_secundario(FILE* arq_base, FILE* ind_prim, char* nome_gerado){
+
+    char linha_atual[TAM_REG];
+    int conta_char;
+
+    inicia_curso *cabeca = (inicia_curso*)malloc(sizeof(inicia_curso));
+    cabeca->primeiro = NULL;
+    cabeca->prox = NULL;
+    
+    while (fgets(linha_atual, TAM_REG, arq_base) != NULL) {
+        for (int i = 0; i < TAM_CHAVE; i++) {
+            string[i] = ' ';
+        }
+        conta_char = 0;
+        for (int i = 0; i < TAM_MATR_NOME && conta_char < TAM_CHAVE; i++) {
+            if (isspace(linha_atual[i])) {
+                continue;
+            }
+            string[conta_char] = linha_atual[i];
+            conta_char++;
+        }
     }
 
     return;
